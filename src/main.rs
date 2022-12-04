fcdb176f2a3f96e80bb07d4bff558be4f358d6d4
jack kingham <wowknead@gmail.com>
2022-12-04 02:22:00 -0500

    Boid window border wraparound.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
         .add_startup_system(init)
         .add_startup_system(spawn_boids)
         .add_system(move_boids)
+        .add_system(boid_window_border_wraparound)
         .run();
 }
 
@@ -25,14 +26,14 @@ fn spawn_boids(mut commands: Commands, assets: Res<AssetServer>) {
     commands.spawn(
         BoidBundle::with_velocity_and_position(
             boid_texture.clone(),
-            vec2(0.5, 0.2),
+            vec2(1., 0.4),
             vec2(-200., 0.)
         )
     );
     commands.spawn(
         BoidBundle::with_velocity_and_position(
             boid_texture.clone(),
-            vec2(-0.3, -0.2),
+            vec2(-0.8, -0.4),
             vec2(200., -100.)
         )
     );
@@ -41,6 +42,22 @@ fn spawn_boids(mut commands: Commands, assets: Res<AssetServer>) {
 fn move_boids(mut boid_query: Query<(&Boid, &mut Transform)>) {
     for (boid, mut boid_transform) in boid_query.iter_mut() {
         boid_transform.translation += Vec3::from((boid.velocity, 0.));
+    }
+}
+
+fn boid_window_border_wraparound(mut boid_query: Query<(&mut Transform, &Sprite), With<Boid>>, windows: Res<Windows>) {
+    let window = windows.get_primary().unwrap();
+    for (mut boid_transform, sprite) in boid_query.iter_mut() {
+        if boid_transform.translation.x+(sprite.custom_size.unwrap().x/2.) < -1.*window.width()/2.
+           || boid_transform.translation.x-(sprite.custom_size.unwrap().x/2.) > window.width()/2.
+        {
+            boid_transform.translation.x *= -1.;
+        }
+        if boid_transform.translation.y+(sprite.custom_size.unwrap().y/2.) < -1.*window.height()/2.
+           || boid_transform.translation.y-(sprite.custom_size.unwrap().y/2.) > window.height()/2.
+        {
+            boid_transform.translation.y *= -1.;
+        }
     }
 }
 
