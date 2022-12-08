e8229cf2c747f2e61d0d1e357928c6699b5312d2
Jack Kingham <wowknead@gmail.com>
2022-12-07 23:10:58 -0500

    Fully working boids simulation.

--- src/boid.rs
+++ src/boid.rs
@@ -2,8 +2,10 @@
 
 use bevy::prelude::*;
 
-const NUM_BOIDS: u8 = 50;
+const NUM_BOIDS: u16 = 150;
 const BOID_RANGE: f32 = 100.;
+const BOID_WIDTH: f32 = 15.;
+const BOID_AVOID_STRENGTH: f32 = 0.02;
 
 #[derive(Component, PartialEq, Debug)]
 pub struct Boid {
@@ -29,6 +31,11 @@ impl Boid {
             .with_system(boid_stick_together)
     }
 
+    pub fn rotate(boid: &mut core::cell::RefMut<(Mut<Boid>, Mut<Transform>)>, radians: f32) {
+        boid.1.rotate_z(radians);
+        boid.0.velocity = Vec2::from_angle(radians).normalize().rotate(boid.0.velocity);
+
+    }
     pub fn steer_towards_point(boid: &mut core::cell::RefMut<(Mut<Boid>, Mut<Transform>)>, point: Vec3, multiplier: f32) {
         //Turns towards point by multiplier amount. 1 makes it steer directly towards point
         let boid_angle = boid.1.rotation.to_euler(EulerRot::XYZ).2;
@@ -39,10 +46,10 @@ impl Boid {
         use std::f32::consts::*;
         if (point_angle-boid_angle+(PI+TAU))%TAU - PI < 0. {
             boid.1.rotate_z(angle_between * multiplier * -1.);
-            boid.0.velocity = Vec2::from_angle(angle_between * multiplier * -1.).rotate(boid.0.velocity);
+            boid.0.velocity = Vec2::from_angle(angle_between * multiplier * -1.).normalize().rotate(boid.0.velocity);
         } else {
             boid.1.rotate_z(angle_between * multiplier);
-            boid.0.velocity = Vec2::from_angle(angle_between * multiplier).rotate(boid.0.velocity);
+            boid.0.velocity = Vec2::from_angle(angle_between * multiplier).normalize().rotate(boid.0.velocity);
         }
     }
     pub fn steer_towards_velocity(boid: &mut core::cell::RefMut<(Mut<Boid>, Mut<Transform>)>, velocity: Vec2, multiplier: f32) {
@@ -66,13 +73,13 @@ pub fn spawn_boids(mut commands: Commands, assets: Res<AssetServer>, windows: Re
     let window = windows.get_primary().unwrap();
 
     use rand::Rng;
+    use bevy::math::vec2;
     let mut rng = rand::thread_rng();
     for _ in 0..NUM_BOIDS {
         let xvel = rng.gen_range(0.3..2.0) * if rand::random() {-1.} else {1.};
         let yvel = rng.gen_range(0.3..2.0) * if rand::random() {-1.} else {1.};
         let xpos = rng.gen_range(window.width()/-2.0 .. window.width()/2.0);
         let ypos = rng.gen_range(window.height()/-2.0 .. window.height()/2.0);
-        use bevy::math::vec2;
         commands.spawn(
             BoidBundle::with_velocity_and_position(
                 boid_texture.clone(),
@@ -110,7 +117,29 @@ fn boid_window_border_wraparound(mut boid_query: Query<(&mut Transform, &Sprite)
 
 fn boid_avoid_others(mut boid_query: Query<(&mut Boid, &mut Transform)>) {
     //Avoid running into other boids
+    use std::{rc::Rc, cell::RefCell};
+    let boid_list: Vec<Rc<RefCell<(Mut<Boid>, Mut<Transform>)>>> = boid_query.iter_mut().map(|b| Rc::new(RefCell::new(b))).collect();
+    for boid_ref in boid_list.iter() {
+        let mut boid = boid_ref.borrow_mut();
+        //these variables represent the line equation ax + by + c = 0
+        let a: f32 = boid.0.velocity.y / boid.0.velocity.x;
+        let b: f32 = -1.;
+        let c: f32 = boid.1.translation.y - (a*boid.1.translation.x);
+        for cmp_boid_ref in boid_list.iter() {
+            if let Ok(cmp_boid) = cmp_boid_ref.try_borrow() {
+                if !boid_is_nearby(&boid.1, &cmp_boid.1, BOID_RANGE)
+                   || (cmp_boid.1.translation - boid.1.translation).angle_between(Vec3::from((boid.0.velocity, 0.))) >= std::f32::consts::FRAC_PI_3*2.
+                   || distance_transform_to_line(a, b, c, &cmp_boid.1) > BOID_WIDTH
+                {
+                    continue;
+                }  
+                let side = side_of_line(boid.1.translation, boid.1.translation+Vec3::from((boid.0.velocity, 0.)), cmp_boid.1.translation);
+                Boid::rotate(&mut boid, BOID_AVOID_STRENGTH * side as f32);
+            }
+        }
+    }
 }
+
 
 fn boid_follow_others(mut boid_query: Query<(&mut Boid, &mut Transform)>) {
     //Steer towards the average heading of nearby boids
@@ -164,7 +193,21 @@ fn boid_is_nearby(transform1: &Mut<Transform>, transform2: &Mut<Transform>, rang
     transform1.translation.distance(transform2.translation) < range
 }
 
+fn distance_transform_to_line(a: f32, b:f32, c:f32, point: &Mut<Transform>) -> f32 {
+    (a*point.translation.x + b*point.translation.y + c).abs() / (a*a + b*b).sqrt()
+}
 
+fn side_of_line(p1: Vec3, p2: Vec3, p3: Vec3) -> i8 {
+    //checks which side of the line created using p1 and p2 p3 lies on
+    //THE LINE IS DIRECTIONAL FROM P1 TO P2
+    //returns -1 for left and 1 for on or right
+    if ((p3.x-p1.x)*(p2.y-p1.y) - (p3.y-p1.y)*(p2.x-p1.x)) > 0. {
+        return 1;
+    } else {
+        return -1;
+    }
+
+}
 
 #[derive(Bundle)]
 pub struct BoidBundle {
@@ -177,7 +220,7 @@ impl BoidBundle {
             sprite: SpriteBundle {
                 texture,
                 sprite: Sprite {
-                    custom_size: Some(Vec2{ x:15., y:20.}),
+                    custom_size: Some(Vec2{ x:BOID_WIDTH, y:20.}),
                     ..default()
                 },
                 ..default()
